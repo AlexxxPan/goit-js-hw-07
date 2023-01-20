2e587596aef706dc56c947dc80b1db62d926b1de
AlexxxPan <alxtouch@gmail.com>
2023-01-20 13:16:49 +0200

    add second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const result = createGallary(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", result);
+
+
+function createGallary(galleryItems) {
+  return galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<a class="gallery__item" href=${original}>
+          <img
+            class="gallery__image"
+            src=${preview}
+            data-source=${original}
+            alt=${description}
+          />
+        </a>`,
+    ""
+  );
+  }
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250, captionsData:"alt"
+})
